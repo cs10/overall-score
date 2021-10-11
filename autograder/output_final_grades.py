e227b7761f45a573dcb69930bbff6caf6e2674b6
Kellyann Ye <46616696+yek3l@users.noreply.github.com>
2021-10-11 14:19:15 -0700

    fixed comma errors

--- autograder/output_final_grades.py
+++ autograder/output_final_grades.py
@@ -15,8 +15,8 @@ gs_output = {'tests': []}
 def output_postlecture_question_grade(grades, gs_output):
 	gs_output['tests'].append({
 		'name': 'Post-Lecture Questions', 
-		'score': float(grades.loc['Post-Lec Qs'])
-		'max_score': 15
+		'score': float(grades.loc['Post-Lec Qs']),
+		'max_score': 15,
 		'output': f"All students are automatically given all Post-Lecture Question points."
 		})
 
