e1f946896172df6f4d6e8f98339627a2061ac32a
Kellyann Ye <46616696+yek3l@users.noreply.github.com>
2021-10-12 14:59:13 -0700

    calling all functions

--- autograder/output_final_grades.py
+++ autograder/output_final_grades.py
@@ -11,7 +11,7 @@ grades = pd.read_csv('/autograder/source/grades.csv', index_col=1).loc[sid]
 
 gs_output = {'tests': []}
 
-def output_postlecture_question_grade(grades, gs_output):
+def output_postlecture_question_score(grades, gs_output):
 	gs_output['tests'].append({
 		'name': 'Post-Lecture Questions', 
 		'score': float(grades.loc['Post-Lec Qs']),
@@ -19,7 +19,7 @@ def output_postlecture_question_grade(grades, gs_output):
 		'output': f"All students are automatically given all Post-Lecture Question points."
 		})
 	
-def output_project_grades(grades, gs_output):
+def output_project_scores(grades, gs_output):
 	project_outputs = [f"Your Project 1 score without lateness deductions is {grades.loc['P1 Actual']}.",
 					   f"Project 1 used {grades.loc['P1 Slip Days']} slip day(s) and was {grades.loc['P1 Days Late']} day(s) late.",
 					   f"Your Project 2 score without lateness deductions is {grades.loc['P2 Actual']}."],
@@ -27,11 +27,11 @@ def output_project_grades(grades, gs_output):
 	gs_output['tests'].append({
         'name': 'Projects',
         'score': float(grades.loc['P1 Actual'] + grades.loc['P2 Actual']),
-        'max_score': 30, #change as more projects are released and graded
+        'max_score': 225, 
         'output': '\n'.join(project _outputs)
     })
 	
-def output_survey_grades(grades, gs_output):
+def output_survey_scores(grades, gs_output):
 	gs_output['tests'].append({
 		'name': 'Weekly Surveys', 
 		'score': float(grades.loc['Surveys Actual']),
@@ -40,17 +40,29 @@ def output_survey_grades(grades, gs_output):
 		})
 		
 	
-def output_lab_grades(grades, gs_output):
-	labs = [f'Lab {i}' for i in range(1, 13 + 1)]
+def output_lab_scores(grades, gs_output):
+	labs = [f'Lab {i}' for i in range(1, 18 + 1)]
     lab_outputs = [f'Your {lab} score is {grades.loc[lab]}.' for lab in labs]
 	lab_outputs = np.append(lab_outputs, [f"Your overall lab score, not counting dropped labs or lateness, is {grades.loc['Lab Actual']}."])
 	gs_output['tests'].append({
 		'name': 'Labs',
 		'score': float(grades.loc['Lab Actual']), 
-		'max_score': 25, 
+		'max_score': 40, 
 		'output': '\n'.join(lab_outputs)
 	})
+	
+def output_quiz_scores(grades, gs_output):
+	quizzes = [f'Week {i} RQ' for i in [2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13]] # these are the weeks we have RQs this semester
+	quiz_outputs = [f'Your {quiz} score is {grades.loc[quiz]}.' for quiz in quizzes]
+	quiz_outputs = np.append(quiz_outputs, [f"Your overall reading quizzes score, not counting dropped quizzes or lateness, is {grades.loc['RQ Actual']}."])
+	gs_output['tests'].append({
+		'name': 'Reading Quizzes',
+		'score': float(grades.loc['RQ Actual'] *1.25), # each rq is worth 1.25 pts
+		'max_score': 10, 
+		'output': '\n'.join(quiz_outputs)
+	})
 
+	
 def output_quest_score(grades, gs_output):
     quest_outputs = [f"Your raw quest score is {grades.loc['Quest Scaled']}."]
     quest_outputs = np.append(quest_outputs, [f"You received {grades.loc['Demo EXC']} extra credit point(s) from completing the quest demo on PL."])
@@ -66,15 +78,18 @@ def output_overall_score(grades, gs_output):
 	gs_output['tests'].append({
 		'name': 'Overall Score',
 		'score': floa(grades.loc['Overall Score']),
-		'max_score': 500, #do math??? cry?? update as more assignments are being released
+		'max_score': 500, 
 		'output': f"Your overall score is {grades.loc['Overall Score']}."
 		})
 
 
 output_overall_score(grades, gs_output)
-output_postlecture_question_grade(grades, gs_output)
-output_lab_grades(grades, gs_output)
-#call all the other ones!
+output_quest_score(grades, gs_output)
+output_project_scores(grades, gs_output)
+output_lab_scores(grades, gs_output)
+output_quiz_scores(grades, gs_output)
+output_survey_scores(grades, gs_output)
+output_postlecture_question_score(grades, gs_output)
 
 out_path = '/autograder/results/results.json'
 with open(out_path, 'w') as f:
